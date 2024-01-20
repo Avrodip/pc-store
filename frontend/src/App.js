2290981dc9846bda017af565ca59cbd9c65ca290
Your Name <kumarsachin22022@gmail.com>
2024-01-20 15:17:48 +0530

    cart done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Cart from './sections/Card/cart';
 function App() {
   return (
     <div className="App">
-    <h1>Hello PC</h1>
+        <Cart />
     </div>
   );
 }
