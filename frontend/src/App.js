9e23c6ec397db377cad6a764b10cf5b408992070
Avrodip <avrodp25@gmail.com>
2024-01-20 22:39:25 +0530

    midsection added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// App.jsx
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Midsection from './components/midsection.jsx'; 
+import Navbar from './sections/navbar.jsx';
+import MainRoutes from './routes/MainRoutes.jsx';
+import Cart from './sections/cart/cart.jsx';
+import DetailsComponent from './components/details.jsx';
 
 function App() {
   return (
-    <div className="App">
-    <h1>Hello PC</h1>
-    </div>
+    <Router>
+      <div className="App" style={{ backgroundColor: 'black', color: 'white' }}>
+        <Navbar />
+        
+        <div>
+          <Cart />
+          <br/>
+        </div>
+
+        <div>
+          <Midsection/>
+        </div>
+        
+        <DetailsComponent/>
+      </div>
+    </Router>
   );
 }
 
