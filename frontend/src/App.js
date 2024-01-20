6449958aa68966b7c75c60d69f1ab6dfbf05edc0
Anurag Dubey <anuragyou4@gmail.com>
2024-01-20 19:29:52 +0530

    navbar and imageslider created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import ImageSlider from './sections/ImageSlider';
+import DrawerAppBar from './sections/Navbar';
 
 function App() {
   return (
     <div className="App">
+       <DrawerAppBar />
+       <ImageSlider/>
     <h1>Hello PC</h1>
     </div>
   );
