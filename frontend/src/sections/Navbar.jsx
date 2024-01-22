33d3748522eedb632903b0ce400f4f088ac85f8a
Anurag Dubey <anuragyou4@gmail.com>
2024-01-23 01:13:23 +0530

    changes in slidebar

--- frontend/src/sections/Navbar.jsx
+++ frontend/src/sections/Navbar.jsx
@@ -6,29 +6,80 @@ import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import SearchIcon from "@mui/icons-material/Search";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 const Navbar = () => {
-  const [anchorEl1, setAnchorEl1] = React.useState(null);
-  const [anchorEl2, setAnchorEl2] = React.useState(null);
+  const [isHovered3, setIsHovered3] = React.useState(false);
+  const [isHovered4, setIsHovered4] = React.useState(false);
 
-  const handleClick = (event, anchorElSetter) => {
-    anchorElSetter(event.currentTarget);
+  const handleMouseEnter = (buttonNumber) => {
+    if (buttonNumber === 3) {
+      setIsHovered3(true);
+    } else if (buttonNumber === 4) {
+      setIsHovered4(true);
+    }
   };
 
-  const handleClose = (anchorElSetter) => {
-    anchorElSetter(null);
+  const handleMouseLeave = (buttonNumber) => {
+    if (buttonNumber === 3) {
+      setIsHovered3(false);
+    } else if (buttonNumber === 4) {
+      setIsHovered4(false);
+    }
+  };
+
+  const renderDropdownContent = (buttonNumber) => {
+    if (buttonNumber === 3 && isHovered3) {
+      return (
+        <div
+          style={{
+            position: "absolute",
+            top: "60px",
+            left: 0,
+            backgroundColor: "#171717",
+            padding: "10px",
+            width: "100%",
+            height: "400px",
+          }}
+          onMouseEnter={() => handleMouseEnter(3)}
+          onMouseLeave={() => handleMouseLeave(3)}
+        >
+          <div>AI & DEEP LEARNING</div>
+          <div>Single GPU Solutions</div>
+          <div>Dual GPU Solutions</div>
+          <div>Quad GPU Solutions</div>
+          <div>Octa GPU Solutions</div>
+        </div>
+      );
+    } else if (buttonNumber === 4 && isHovered4) {
+      return (
+        <div
+          style={{
+            position: "absolute",
+            top: "60px",
+            left: 0,
+            backgroundColor: "#171717",
+            padding: "10px",
+            width: "100%",
+            height: "400px",
+          }}
+          onMouseEnter={() => handleMouseEnter(4)}
+          onMouseLeave={() => handleMouseLeave(4)}
+        >
+          <div>TRADING PC</div>
+          <div>2 Display Solution</div>
+          <div>4 Display Solution</div>
+          <div>8 Display Solution</div>
+        </div>
+      );
+    }
+    return null;
   };
 
   return (
-    <div>
+    <Grid style={{ margin: 0, padding: 0 }}>
       {/* First Navbar */}
-      <AppBar
-        position="static"
-        style={{ backgroundColor: "#171717", height: "35px" }}
-      >
+      <AppBar position="fixed" style={{ backgroundColor: "#171717", height: "35px" }}>
         <Toolbar>
           <Grid container alignItems="center" justifyContent="space-between">
             {/* Logo on the Top-Left Corner */}
@@ -40,12 +91,16 @@ const Navbar = () => {
             <Grid
               item
               container
-              xs={4}
+              xs={8}
               justifyContent="flex-end"
               style={{ marginBottom: "25px" }}
             >
-              <Button color="inherit">Register</Button>
-              <Button color="inherit">Login</Button>
+              <Button color="inherit" onClick={() => {}}>
+                Register
+              </Button>
+              <Button color="inherit" onClick={() => {}}>
+                Login
+              </Button>
               <IconButton color="inherit">
                 <SearchIcon />
               </IconButton>
@@ -58,115 +113,69 @@ const Navbar = () => {
       </AppBar>
 
       {/* Second Navbar */}
-      <AppBar
-        position="static"
-        color="primary"
-        style={{ backgroundColor: "#000000" }}
-      >
+      <AppBar position="fixed" color="primary" style={{ backgroundColor: "#000000", top: '35px' }}>
         <Toolbar>
           <Grid container alignItems="center" justifyContent="space-between">
             {/* Logo on the Top-Left Corner */}
             <Grid item>
               <img
-                src="logo.png"
+                src="/Images/Logo-Idea-white.png"
                 alt="Logo"
                 className="logo"
-                style={{ width: "70px", height: "auto" }}
+                style={{ width: "50px", height: "auto" }}
               />
             </Grid>
 
-             {/* Buttons with Dropdowns */}
-             <Grid item container xs={4} justifyContent="flex-end">
+            {/* Buttons with Dropdowns */}
+            <Grid
+              item
+              container
+              xs={8}
+              justifyContent="flex-end"
+            >
               <Button
                 color="inherit"
-                aria-controls="dropdown1"
-                aria-haspopup="true"
-                onClick={(event) => handleClick(event, setAnchorEl1)}
+                onClick={() => renderDropdownContent(3)}
+                onMouseEnter={() => handleMouseEnter(3)}
+                onMouseLeave={() => handleMouseLeave(3)}
+                style={{
+                  color: isHovered3 ? "red" : "inherit",
+                  textDecoration: isHovered3 ? "underline" : "inherit",
+                }}
               >
                 Creation
                 <ArrowDropDownIcon />
               </Button>
-              <Menu
-                id="dropdown1"
-                anchorEl={anchorEl1}
-                keepMounted
-                open={Boolean(anchorEl1)}
-                onClose={() => handleClose(setAnchorEl1)}
-              >
-                <MenuItem onClick={() => handleClose(setAnchorEl1)}>Option 1</MenuItem>
-                <MenuItem onClick={() => handleClose(setAnchorEl1)}>Option 2</MenuItem>
-                <MenuItem onClick={() => handleClose(setAnchorEl1)}>Option 3</MenuItem>
-              </Menu>
+              {renderDropdownContent(3)}
 
               <Button
                 color="inherit"
-                aria-controls="dropdown2"
-                aria-haspopup="true"
-                onClick={(event) => handleClick(event, setAnchorEl2)}
+                onClick={() => renderDropdownContent(4)}
+                onMouseEnter={() => handleMouseEnter(4)}
+                onMouseLeave={() => handleMouseLeave(4)}
+                style={{
+                  color: isHovered4 ? "red" : "inherit",
+                  textDecoration: isHovered4 ? "underline" : "inherit",
+                }}
               >
                 Gaming
                 <ArrowDropDownIcon />
               </Button>
-              <Menu
-                id="dropdown2"
-                anchorEl={anchorEl2}
-                keepMounted
-                open={Boolean(anchorEl2)}
-                onClose={() => handleClose(setAnchorEl2)}
-              >
-                <MenuItem onClick={() => handleClose(setAnchorEl2)}>Option 1</MenuItem>
-                <MenuItem onClick={() => handleClose(setAnchorEl2)}>Option 2</MenuItem>
-                <MenuItem onClick={() => handleClose(setAnchorEl2)}>Option 3</MenuItem>
-              </Menu>
+              {renderDropdownContent(4)}
 
-              <Button
-                color="inherit"
-                aria-controls="dropdown3"
-                aria-haspopup="true"
-                onClick={(event) => handleClick(event, setAnchorEl2)}
-              >
+              <Button color="inherit" onClick={() => {}}>
                 DIY PC
                 <ArrowDropDownIcon />
               </Button>
-              <Menu
-                id="dropdown3"
-                anchorEl={anchorEl2}
-                keepMounted
-                open={Boolean(anchorEl2)}
-                onClose={() => handleClose(setAnchorEl2)}
-              >
-                <MenuItem onClick={() => handleClose(setAnchorEl2)}>Option 1</MenuItem>
-                <MenuItem onClick={() => handleClose(setAnchorEl2)}>Option 2</MenuItem>
-                <MenuItem onClick={() => handleClose(setAnchorEl2)}>Option 3</MenuItem>
-              </Menu>
-
-              <Button
-                color="inherit"
-                aria-controls="dropdown4"
-                aria-haspopup="true"
-                onClick={(event) => handleClick(event, setAnchorEl2)}
-              >
+              <Button color="inherit" onClick={() => {}}>
                 Peripherals
                 <ArrowDropDownIcon />
               </Button>
-              <Menu
-                id="dropdown4"
-                anchorEl={anchorEl2}
-                keepMounted
-                open={Boolean(anchorEl2)}
-                onClose={() => handleClose(setAnchorEl2)}
-              >
-                <MenuItem onClick={() => handleClose(setAnchorEl2)}>Option 1</MenuItem>
-                <MenuItem onClick={() => handleClose(setAnchorEl2)}>Option 2</MenuItem>
-                <MenuItem onClick={() => handleClose(setAnchorEl2)}>Option 3</MenuItem>
-              </Menu>
-
-              {/* Add more buttons with dropdowns as needed */}
             </Grid>
           </Grid>
         </Toolbar>
       </AppBar>
-    </div>
+    </Grid>
   );
 };
 
