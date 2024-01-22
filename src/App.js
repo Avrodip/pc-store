6609b845b67a1d83254eda33fa818a95a9d25b48
akash368 <akashmaurya368@gmail.com>
2024-01-22 08:37:43 +0530

    adding some files

---
+++ src/App.js
@@ -0,0 +1,31 @@
+// App.jsx
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Midsection from './sections/midsection.jsx'; 
+import Navbar from './sections/navbar.jsx';
+import MainRoutes from './routes/MainRoutes.jsx';
+import Cart from './sections/cart/cart.jsx';
+import DetailsComponent from './sections/details.jsx';
+import Quotes from './sections/quotes.jsx';
+import FocusCart from './sections/focusCart.jsx';
+import ImageSlider from './sections/ImageSlider.jsx';
+import Footer from './sections/footer.js';
+
+function App() {
+  return (
+    <Router>
+      <div className="App" style={{ backgroundColor: 'black', color: 'white' }}>
+        <Navbar />
+        <ImageSlider />
+        <Cart />
+        <Midsection />
+        <DetailsComponent />
+        <FocusCart />
+        <Quotes />
+        <Footer/>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
