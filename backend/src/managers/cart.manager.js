952c45c2c106fcb770bb4e4bfcc00a6a4184d7d1
Your Name <kumarsachin22022@gmail.com>
2024-02-09 17:22:37 +0530

    push backend

---
+++ backend/src/managers/cart.manager.js
@@ -0,0 +1,107 @@
+const db = require("../config/database");
+
+class CartManager {
+
+    async getCartList(req, res) {
+        try {
+            if (!db) {
+                throw new Error("Database object is undefined");
+            }
+            const [rows, fields] = await db.promise().query('CALL getCartList()');
+            return rows;
+        } catch (error) {
+            console.error("Error occurred:", error);
+            throw error;
+        }
+    }
+    async updateCart(req, res) {
+        try {
+            const {
+                actionType,
+                id,
+                processor,
+                motherBoard,
+                ram,
+                ramQuantity,
+                graphicCard,
+                graphicCardQuantity,
+                ssd,
+                ssdQuantity,
+                hdd,
+                hddQuantity,
+                price,
+                quantity,
+                primaryStorage,
+                secondaryStorage,
+                casse,
+                cpuCooler,
+                psu,
+                support,
+                os,
+                monitor,
+                keyboard,
+                mouse,
+                wifi,
+                customCable,
+                user,
+                productType,
+            } = req.body;
+            if (!db) {
+                throw new Error("Database object is undefined");
+            }
+
+            const [rows, fields] = await db.promise().execute('CALL updateCart(?,?,?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                [
+                    actionType,
+                    id,
+                    processor,
+                    motherBoard,
+                    ram,
+                    ramQuantity,
+                    graphicCard,
+                    graphicCardQuantity,
+                    ssd,
+                    ssdQuantity,
+                    hdd,
+                    hddQuantity,
+                    price,
+                    quantity,
+                    primaryStorage,
+                    secondaryStorage,
+                    casse,
+                    cpuCooler,
+                    psu,
+                    support,
+                    os,
+                    monitor,
+                    keyboard,
+                    mouse,
+                    wifi,
+                    customCable,
+                    user,
+                    productType,
+                ]
+            );
+            return rows;
+        } catch (error) {
+            console.error("Error occurred:", error);
+            throw error;
+        }
+    }
+    async deleteCart(req, res) {
+        try {
+            const id = req.body.id;
+            if (!db) {
+                throw new Error("Database object is undefined");
+            }
+
+            const [rows, fields] = await db.promise().query('CALL deleteCart(?)', [id]);
+            return rows;
+        } catch (error) {
+            console.error("Error occurred:", error);
+            throw error;
+        }
+    }
+}
+
+module.exports = { CartManager };
--- backend/src/routes/api.routes.js
+++ backend/src/routes/api.routes.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const authRoutes = require("./auth/auth.routes")
 const processorRoutes = require("./processor/processor.routes");
-
+const cartRoutes =  require("./cart/cart.routes")
 const app = express()
 
 app.use("/auth/", authRoutes)
 app.use("/processor/", processorRoutes)
-
+app.use("/cart/",cartRoutes);
 
 module.exports = app;
