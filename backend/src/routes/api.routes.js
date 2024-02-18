6da3cb14980cd76423a67bef6b13701b2bde44b3
Your Name <kumarsachin22022@gmail.com>
2024-02-18 09:36:27 +0530

    merge with payment

--- backend/src/routes/api.routes.js
+++ backend/src/routes/api.routes.js
@@ -4,6 +4,7 @@ const processorRoutes = require("./processor/processor.routes");
 const cartRoutes =  require("./cart/cart.routes")
 const billingAddressRoutes = require("./address/billingAddress.routes")
 const shippingAddressRoutes = require("./address/shippingAddress.routes")
+const paymentRoutes = require('../routes/payment/payment.rotue')
 const app = express()
 
 app.use("/auth/", authRoutes)
@@ -11,4 +12,5 @@ app.use("/processor/", processorRoutes)
 app.use("/cart/",cartRoutes);
 app.use("/billingAddress/",billingAddressRoutes);
 app.use("/shippingAddress/",shippingAddressRoutes)
+app.use("/payment/",paymentRoutes)
 module.exports = app;
